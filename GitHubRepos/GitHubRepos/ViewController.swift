6d3481efedb058450613ee2136a81aff8bf93119
Tiago Chaves <tchaves74@gmail.com>
2019-07-27 02:36:15 -0300

    Criação do projeto e configuração dos pods: - Alamofire para requests; - Cartography para auxiliar no viewcode.

---
+++ GitHubRepos/GitHubRepos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubRepos
+//
+//  Created by Tiago Chaves on 27/07/19.
+//  Copyright © 2019 Tiago Chaves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
